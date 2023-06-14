558a54613fc0c47d75f6b5f2d8ff9d5026a9dffa
mengxue.wu <mengxue.wu@faw-vw.com>
2023-06-14 11:12:02 +0800

    first commit

---
+++ src/test/java/com/example/privatechatgpt/PrivateChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.privatechatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateChatGptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
